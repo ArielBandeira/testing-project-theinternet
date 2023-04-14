c538578b20581e8fe724f5f55380f4629f5b879e
ariel <arieltwain@gmail.com>
2023-04-14 13:40:16 -0300

    adding test utilities

--- src/main/java/com/herkuapp/theinternet/base/BaseTest.java
+++ src/main/java/com/herkuapp/theinternet/base/BaseTest.java
@@ -2,18 +2,27 @@ package com.herkuapp.theinternet.base;
 
 import org.openqa.selenium.WebDriver;
 import org.apache.logging.log4j.Logger;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.lang.reflect.Method;
 
 public class BaseTest {
 
     protected WebDriver driver;
     protected Logger log;
+    protected String testSuiteName;
+    protected String testName;
+    protected String testMethodName;
 
     //SetUp
     @BeforeMethod( alwaysRun = true )
-    public void setUp() {
+    public void setUp(Method method, ITestContext ctx) {
         driver.manage().window().maximize();
+        this.testSuiteName = ctx.getSuite().getName();
+        this.testName = testName;
+        this.testMethodName = method.getName();
     }
 
     //TearDown
