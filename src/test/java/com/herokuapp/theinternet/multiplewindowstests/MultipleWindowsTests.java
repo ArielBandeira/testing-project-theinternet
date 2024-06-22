5a8507f41526b216961a48847fae142784d7d134
arieltwain <arieltwain@gmail.com>
2024-06-22 16:44:57 -0300

    refactoring MultipleWindows

--- src/test/java/com/herokuapp/theinternet/multiplewindowstests/MultipleWindowsTests.java
+++ src/test/java/com/herokuapp/theinternet/multiplewindowstests/MultipleWindowsTests.java
@@ -6,48 +6,30 @@ import com.herokuapp.theinternet.pages.NewWindowPage;
 import com.herokuapp.theinternet.pages.WelcomePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import java.io.IOException;
 
 public class MultipleWindowsTests extends TestUtilities {
 
     //region Variables
-    String expectedTitle = "New Window";
-    String expectedURL = "https://the-internet.herokuapp.com/windows/new";
-
     WelcomePage welcomePage;
     //endregion
 
-    //TODO Breakdown this test into multiple tests
-    // like verify window title, verify new window text, verify new window url etc
-    //Click on 'Click Here' link to open new page
     @Test
-    public void clickNewWindowLink() {
+    public void verifyNewWindowLinkOpensCorrectPageUrl() throws IOException {
 
         welcomePage = new WelcomePage(driver, log);
-
-        //Open Opening a new window page
         MultipleWindowsPage multipleWindowsPage = welcomePage.clickMultipleWindowsLink();
 
-        //Click on 'Click Here' link to open new page
-        log.info("Click on 'Click Here' link to open new window");
+
         multipleWindowsPage.clickNewWindowLink();
 
-        //Switch to new window
-        log.info("Switch to window with title: New Window");
-        NewWindowPage newWindow = welcomePage.switchToNewWindow();
+        String currentPageUrl = driver.getCurrentUrl();
+        String expectedPageUrl = readCSVFile().get(0);
 
-        //Check new window page title
-        log.info("Get new window title");
-        String actualTitle = newWindow.getCurrentWindowTitle();
+        log.info("Verify if page URL is the expected");
+        Assert.assertEquals(currentPageUrl, expectedPageUrl, "Actual page URL dos not match the expected");
 
-        //Check if new window title is the expected
-        log.info("Check if new window title is the expected");
-        Assert.assertEquals(actualTitle, expectedTitle, "New window title is not the expected");
-        log.info("New window expected title: " + actualTitle);
-        log.info("New window actual title: " + actualTitle);
-
-        //Get new window URL
-        log.info("Get new window URL");
-        String actualURL = newWindow.getCurrentPageUrl();
+    }
 
         //Check if new window URL if the expected
         log.info("Check if the new window URL is the expected");
